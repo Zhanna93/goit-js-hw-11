491106914510e092f0b20ee983aa9f1ded141f45
Zhanna93 <121199582+Zhanna93@users.noreply.github.com>
2023-06-06 14:40:36 +0300

    1

--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,35 +1,3 @@
-// import axios from 'axios';
-
-//   const API_KEY = '37041960-f82a2a53b8b1784f2737de35d';
-//   const BASE_URL = 'https://pixabay.com/api/';
-
-//   // page = 1;
-//   // per_page = 40;
-//   // q = null;
-
-// async function fetchPhoto(keyword, page) {
-//   try {
-//    const { data } = await axios("events.json", {
-//     params: {
-//      key: API_KEY,
-//      keyword,
-//      image_type: 'photo',
-//      orientation: 'horizontal',
-//      safesearch: 'true',
-//      page,
-//      per_page: 40,
-//      q: null,
-//     },
-//    });
-//    return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
- 
-// export { fetchPhoto };
-
- 
  import axios from 'axios';
 
 export class PixabayAPI {
